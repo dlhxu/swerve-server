1ce9b84abf698dba4c0280f4876181de8370af88
Derek Xu <derekxu.uhs@gmail.com>
2019-04-17 16:28:14 -0400

    refactored callback functions, parameterized query to BigQuery

---
+++ apiController.js
@@ -0,0 +1,52 @@
+// Import the Google Cloud client library
+const {BigQuery} = require('@google-cloud/bigquery');
+
+class ApiController {
+  async queryBQHelper(payload){
+    // Create a client
+    const bigqueryClient = new BigQuery();
+    console.log(payload);
+    const sqlQuery = `SELECT city08, highway08
+  	 FROM \`hack-lassonde.hack_lassonde.vehicle_data\`
+  	 WHERE make = @make
+  	 AND model = @model
+  	 AND year = @year
+     AND trany = @trany
+     AND drive = @drive`;
+
+    const options = {
+      query: sqlQuery,
+      params: {
+        make: payload.make,
+        model: payload.model,
+        year: payload.year,
+        trany: payload.trany,
+        drive: payload.drive
+      }
+    };
+    console.log(options.params);
+    // Run the query
+    const [rows] = await bigqueryClient.query(options);
+    var cityMileages = [];
+    var highwayMileages = [];
+    console.log('Query Results:');
+
+    // query results will only be 1 row
+    rows.forEach(row => {
+      cityMileages.push(row['city08']);
+      highwayMileages.push(row['highway08']);
+      console.log(`city mileage: ${cityMileages}, highway mileage: ${highwayMileages}`);
+    });
+
+    const retData = {
+      cityMileage: cityMileages[0],
+      highwayMileage: highwayMileages[0],
+    };
+
+    console.log(retData);
+    return retData;
+  }
+}
+
+const apiController = new ApiController();
+export default apiController;
--- index.js
+++ index.js
@@ -1,30 +1,24 @@
-const {BigQuery} = require('@google-cloud/bigquery');
+import express from 'express';
+import apiController from './apiController';
+import bodyParser from 'body-parser';
 
-async function queryBQ(){
-  // Create a client
-  const bigqueryClient = new BigQuery();
+const router = express.Router();
 
-  const sqlQuery = `SELECT city08, highway08
-	 FROM \`hack-lassonde.hack_lassonde.vehicle_data\`
-	 WHERE make = "Toyota"
-	 AND model = "Camry"
-	 AND year = 2018
-   AND trany = "Automatic (S8)"
-   AND drive = "Front-Wheel Drive"`;
+router.use(bodyParser.json());
+router.use(bodyParser.urlencoded({ extended: false }));
 
-  const options = {
-    query: sqlQuery,
-  };
+// get all todos
+router.get('/api/swerve-vehicle-data', async (req, res) => {
+  const queryData = await apiController.queryBQHelper(req.body);
+  const city = queryData.cityMileage;
+  const highway = queryData.highwayMileage;
+  console.log(queryData);
 
-  // Run the query
-  const [rows] = await bigqueryClient.query(options);
-
-  console.log('Query Results:');
-  rows.forEach(row => {
-    const city08 = row['city08'];
-    const highway08 = row['highway08'];
-    console.log(`city mileage: ${city08}, highway mileage: ${highway08}`);
+  res.status(200).send({
+    cityMileage: city,
+    highwayMileage: highway,
   });
-}
 
-queryBQ();
+});
+
+export default router;
